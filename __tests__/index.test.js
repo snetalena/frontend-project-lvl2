064124e238bf2be810816f3c8f247575635aae84
snetalena <snetalena@gmail.com>
2019-12-16 15:03:31 +0300

    refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,20 +2,17 @@ import genDiff from '../src/index';
 
 const fs = require('fs');
 
+const expectedFile = `${__dirname}/__fixtures__/expected.txt`;
+const expectedResult = fs.readFileSync(expectedFile, 'utf-8');
+
 test('test for genDiff, format json', () => {
-  console.log(__dirname);
   const firstFile = `${__dirname}/__fixtures__/before.json`;
   const secondFile = `${__dirname}/__fixtures__/after.json`;
-  const expectedFile = `${__dirname}/__fixtures__/expected.txt`;
-  const expectedResult = fs.readFileSync(expectedFile, 'utf-8');
   expect(genDiff(firstFile, secondFile)).toEqual(expectedResult);
 });
 
 test('test for genDiff, format yaml', () => {
-  console.log(__dirname);
   const firstFile = `${__dirname}/__fixtures__/yaml/before.yml`;
   const secondFile = `${__dirname}/__fixtures__/yaml/after.yml`;
-  const expectedFile = `${__dirname}/__fixtures__/expected.txt`;
-  const expectedResult = fs.readFileSync(expectedFile, 'utf-8');
   expect(genDiff(firstFile, secondFile)).toEqual(expectedResult);
 });
