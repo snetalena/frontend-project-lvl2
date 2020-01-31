190884d16fb8bc072b1df68c49bd46cb69be891b
snetalena <snetalena@gmail.com>
2020-01-31 13:45:50 +0300

    refactor

--- src/formatters/renderPlain.js
+++ src/formatters/renderPlain.js
@@ -7,20 +7,22 @@ const valueToString = (value) => {
   return typeof value === 'string' ? `'${value}'` : value;
 };
 
+const propertyNamesToString = (pnames) => (pnames.length > 1 ? pnames.join('.') : pnames);
+
 const renderPlain = (ast) => {
   const iter = (tree, propertyNames) => {
     const buildLine = {
-      added: (node, propertyName) => `Property '${propertyName.length > 1 ? propertyName.join('.') : propertyName}' was added with value: ${valueToString(node.newValue)}`,
-      removed: (node, propertyName) => `Property '${propertyName.length > 1 ? propertyName.join('.') : propertyName}' was removed`,
-      updated: (node, propertyName) => `Property '${propertyName.length > 1 ? propertyName.join('.') : propertyName}' was updated. From ${valueToString(node.oldValue)} to ${valueToString(node.newValue)}`,
-      hasChildren: (node, propertyName) => iter(node.children, propertyName),
-      unchanged: () => '',
+      added: (node, pnames) => `Property '${propertyNamesToString(pnames)}' was added with value: ${valueToString(node.newValue)}`,
+      removed: (node, pnames) => `Property '${propertyNamesToString(pnames)}' was removed`,
+      updated: (node, pnames) => `Property '${propertyNamesToString(pnames)}' was updated. From ${valueToString(node.oldValue)} to ${valueToString(node.newValue)}`,
+      hasChildren: (node, pnames) => iter(node.children, pnames),
+      unchanged: () => null,
     };
     const result = tree.map((node) => {
       const pnames = [...propertyNames, node.keyName];
       return buildLine[node.difference](node, pnames);
     });
-    return result.filter((item) => item !== '').join('\n');
+    return result.filter((item) => item !== null).join('\n');
   };
   return iter(ast, []);
 };
